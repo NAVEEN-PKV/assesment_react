186f77cb7ed2a57774a1cc9b68b6642ac3e22a00
naveen <naveen.prakashkv@ust.com>
2025-09-26 13:25:48 +0530

    test: add button click test to Navigation component

--- src/__test__/Navigation.test.tsx
+++ src/__test__/Navigation.test.tsx
@@ -1,11 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen,fireEvent } from '@testing-library/react'
 import Navigation from '../components/Navigation/Navigation'
 import { describe, it,expect } from 'vitest'
+import { NavigationData } from '../data/NavigationData'
 
 describe('Button', () => {
   it('should click the button ', () => {
     render(<Navigation  />)
     expect(screen.getByText("Introduction to React")).toBeInTheDocument();
   })
+  it("updates buttonId when a button is clicked", () => {
+    render(<Navigation />);
+    const secondButton = screen.getByText(NavigationData[1].label);
+    fireEvent.click(secondButton);
+    expect(screen.getByText(NavigationData[1].heading)).toBeInTheDocument();
+  });
  
 })
