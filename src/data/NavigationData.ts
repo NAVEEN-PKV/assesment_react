fbbc067bab22d9c2e7f4757a6e6563092fa9ac5c
naveen <naveen.prakashkv@ust.com>
2025-09-26 13:21:49 +0530

    fix: update labels in NavigationData for consistency and clarity

--- src/data/NavigationData.ts
+++ src/data/NavigationData.ts
@@ -3,25 +3,25 @@ import type { NavigationDataTypes } from "../types/NavigationDataTypes";
 export const NavigationData: NavigationDataTypes[] = [
   {
     id: 1,
-    label: "intro",
+    label: "Introduction",
     heading: "Introduction to React",
     content: "React is a JavaScript library for building user interfaces."
   },
   {
     id: 2,
-    label: "hooks",
+    label: "Hooks",
     heading: "Understanding Hooks",
     content: "Hooks let you use state and lifecycle features in functional components."
   },
   {
     id: 3,
-    label: "testing",
+    label: "Testing",
     heading: "Testing React Apps",
     content: "You can test React apps using tools like React Testing Library,Jest and Vitest."
   },
   {
     id: 4,
-    label: "summary",
+    label: "Summary",
     heading: "Summary",
     content: "React makes it easier to build interactive and reusable UI components."
   }
