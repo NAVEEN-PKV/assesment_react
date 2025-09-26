66fd32c06b7a5b812b79294b065035807eba4734
naveen <naveen.prakashkv@ust.com>
2025-09-26 13:10:08 +0530

    refactor: remove unused import from Navigation test

--- src/__test__/Navigation.test.tsx
+++ src/__test__/Navigation.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Navigation from '../components/Navigation/Navigation'
-import { describe, it,expect, vi } from 'vitest'
+import { describe, it,expect } from 'vitest'
 
 describe('Button', () => {
   it('should click the button ', () => {
